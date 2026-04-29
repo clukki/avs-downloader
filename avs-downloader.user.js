88ea08595e5550912a5b82715fda4a0ea7acb022
clukki <nguyenhuuminh16911@gmail.com>
2026-04-29 17:34:35 +0700

    change domain

--- avs-downloader.user.js
+++ avs-downloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Avs downloader
 // @namespace   avs_downloader
-// @match       https://animevietsub.id/phim/*/*.html
+// @match       https://animevietsub.bz/phim/*/*.html
 // @grant       GM.download
 // @version     1.0
 // @author      clukki
